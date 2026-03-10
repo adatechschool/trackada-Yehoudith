e75e4ac9868ced06749206ba5ed03826d3cf3ee1
Yehoudith <yehoudith22@hotmail.fr>
2026-03-10 21:19:24 +0100

    existe ou n'existe pas

--- checktrack.js
+++ checktrack.js
@@ -1,4 +1,4 @@
-import {readFileSync, existsSync} from "fs";
+import {readFileSync, existsSync, readdirSync} from "fs";
 import {join} from "path";
 import {homedir} from "os";
 
@@ -6,6 +6,11 @@ const track = JSON.parse(readFileSync("./track.json"));
 const root = track.root.replace("~", homedir());
 
 for (const {name, required} of track.projects) {
-    const projectExists = existsSync(join(root, name));
-    console.log(projectExists ? "✅" : "❌", join(root, name), required.length);
+    const projectP = join(root, name);
+
+    if(existsSync(projectP)){
+        console.log(name, "existe");
+    }
+
+    else { console.log(name, "n'existe pas") };
 }
